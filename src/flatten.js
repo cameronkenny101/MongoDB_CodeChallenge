71853f21a08c66fa9e3906dff999e7f694fd223f
cameronkenny101 <cam.e.kenny@gmail.com>
2021-03-23 11:40:54 +0000

    Added extra detail to solution

--- src/flatten.js
+++ src/flatten.js
@@ -1,5 +1,6 @@
 console.log("Please enter your JSON string (press ctrl D to submit json): \n")
 
+// Standard input handler
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 let input = '';
@@ -10,9 +11,21 @@ process.stdin.on('data', function (chunk) {
 
 process.stdin.on('end', function () {
     const parsedData = JSON.parse(input);
+    // Call and prettify our flattened JSON
     const ans = JSON.stringify(flatten(parsedData), null, "   ");
     console.log("Your JSON: \n" + ans);
 });
+
+/*
+ * Recursive function to flatten a JSON input. We go through each object
+ * in the JSON. If there is an object inside an object, we recursively flatten
+ * the object until we reach our base case which is when the JSON has been flattened.
+ * We then return the flattened JSON when we have went through each elemtn and it has
+ * been flattened.
+ *
+ * @param json: This is our json input
+ * @return We return the flattened version of the json input
+ */
 
 function flatten (json) {
     const flattened = {};
@@ -20,10 +33,13 @@ function flatten (json) {
     let value;
     for (const i in json) {
 
+        // Stops us from having duplicate keys. According to "The JavaScript Object Notation (JSON) Data Interchange Format",
+        // we are not supposed to have duplicate keys: https://tools.ietf.org/html/rfc8259#section-4
         if (!json.hasOwnProperty(i))
             continue;
 
         if ((typeof json[i]) === 'object') {
+            // Recursively flatten the json
             value = flatten(json[i]);
             for (const j in value) {
                 if (!value.hasOwnProperty(j))
@@ -34,6 +50,7 @@ function flatten (json) {
             flattened[i] = json[i];
         }
     }
+
     return flattened;
 }
 
